79212e1383599bae4f961c8fb3fe803222240219
piyush <21.piyushgupta@gmail.com>
2023-12-27 12:59:03 -0500

    Added PP Custom Framework

--- Package.swift
+++ Package.swift
@@ -9,15 +9,11 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "CustomFramework",
-            targets: ["CustomFramework"]),
+            targets: ["PPCustomFramework"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "CustomFramework"),
-        .testTarget(
-            name: "CustomFrameworkTests",
-            dependencies: ["CustomFramework"]),
+        .binaryTarget(name: "PPCustomFramework", path: "./sources/PPCustomFramework.xcframework")
     ]
 )
