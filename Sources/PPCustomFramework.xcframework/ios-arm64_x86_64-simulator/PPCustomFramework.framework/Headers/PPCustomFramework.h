61c6cd9e6a97588450e70c9069ae663fbb747de9
piyush <21.piyushgupta@gmail.com>
2023-12-27 12:58:36 -0500

    Added PPCustomFramework

---
+++ Sources/PPCustomFramework.xcframework/ios-arm64_x86_64-simulator/PPCustomFramework.framework/Headers/PPCustomFramework.h
@@ -0,0 +1,18 @@
+//
+//  PPCustomFramework.h
+//  PPCustomFramework
+//
+//  Created by Piyush on 12/27/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PPCustomFramework.
+FOUNDATION_EXPORT double PPCustomFrameworkVersionNumber;
+
+//! Project version string for PPCustomFramework.
+FOUNDATION_EXPORT const unsigned char PPCustomFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PPCustomFramework/PublicHeader.h>
+
+
