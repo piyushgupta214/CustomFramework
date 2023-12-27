de3cc1e48eeff78fbc90a2278149a11ec824072f
piyush <21.piyushgupta@gmail.com>
2023-12-27 11:59:20 -0500

    added functionality

--- Sources/CustomFramework/CustomFramework.swift
+++ Sources/CustomFramework/CustomFramework.swift
@@ -1,2 +1,10 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+public struct CustomFrameworkSDK {
+    
+    public func initSDK(){
+        print("Your Custom framework SDK is initiated successfully")
+    }
+    
+}
